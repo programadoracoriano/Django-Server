49120ba6f433bd05e0175bb3af91e62809205291
Gualter Perinho <gualterperinho@iMac-de-Gualter.Home>
2023-03-12 11:08:00 -0100

    gunicorn

--- mysite/settings.py
+++ mysite/settings.py
@@ -172,5 +172,3 @@ MEDIA_DIRS = [os.path.join(BASE_DIR, "media")]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CSRF_TRUSTED_ORIGINS = ['https://django-server-production-fb0b.up.railway.app']
