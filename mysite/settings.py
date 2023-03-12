85b446af28ebfdb4246330ebaeb85010f3c05be1
Gualter Perinho <gualterperinho@iMac-de-Gualter.Home>
2023-03-12 10:59:21 -0100

    adding origins csrf

--- mysite/settings.py
+++ mysite/settings.py
@@ -172,3 +172,5 @@ MEDIA_DIRS = [os.path.join(BASE_DIR, "media")]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['*']
